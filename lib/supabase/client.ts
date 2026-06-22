07e8669a26557b71aea01b4549fee5294de5607a
met3or <ththach.tran@outlook.com>
2026-06-23 00:16:10 +0200

    add: Supabase client setup

---
+++ lib/supabase/client.ts
@@ -0,0 +1,9 @@
+//.Used in frontend pages to read data from Supabase
+import { createBrowserClient } from "@supabase/ssr";
+
+export function createClient() {
+    return createBrowserClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+}
---
+++ lib/supabase/server.ts
@@ -0,0 +1,9 @@
+//.This file is used in API routes to write data to Supabase.
+import { createClient } from '@supabase/supabase-js'
+
+export function createAdminClient() {
+    return createClient(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.SUPABASE_SERVICE_ROLE_KEY!    
+    )
+}
